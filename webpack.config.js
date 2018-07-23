e681c5ac0cd554743269563ee436572ca01f91a9
Дутченко Олег <dutchenko.o.wezom@gmail.com>
2018-07-23 19:41:45 +0300

    SVGSpritemapPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,9 @@
 // Imports
 // ----------------------------------------
 
-const webpack = require('webpack');
 const path = require('path');
+const webpack = require('webpack');
+const SVGSpritemapPlugin = require('svg-spritemap-webpack-plugin');
 
 // ----------------------------------------
 // Private
@@ -30,7 +31,20 @@ const webpackConfig = {
 		path: path.resolve(_cwd, './public/assets/js/'),
 		publicPath: '/public/assets/',
 		chunkFilename: '_async-modules/[name].js?v=[chunkhash]'
-	}
+	},
+	plugins: [
+		new SVGSpritemapPlugin({
+			src: 'src/svg/**/*.svg',
+			filename: 'sprite.svg',
+			gutter: 5,
+			prefix: 'sprite-',
+			svgo: {
+				plugins: [{
+					removeTitle: true
+				}]
+			}
+		})
+	]
 };
 
 // ----------------------------------------
